009dec3b23350fcd998164c27821883c52a9f0b1
MonikaMsk <monika.musak@gmail.com>
2017-10-02 08:45:00 +0200

    Some changes

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ $( "li" )
     .filter(function( index ) {
         return $( ".age", this ).length === 1;
     })
+
+$('li.active').on('mouseenter', function() {
+    $(this).superslides('stop');
+});
+$('li.active').on('mouseleave', function() {
+    $(this).superslides('start');
+});
